3232b509c976c2317ff7f1db5f694b4684d9ab63
netanelf <netanelf@cs.huji.ac.il>
2018-06-16 22:21:38 +0300

    initial client struct

--- whatsappClient.cpp
+++ whatsappClient.cpp
@@ -8,6 +8,12 @@
 
 //// ===========================   Typedefs & Structs =============================================
 
+// client
+struct Client
+{
+    std::string name;
+    int sockfd;
+};
 
 //// ===========================   Global Variables ===============================================
 
@@ -19,6 +25,21 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 4) {
+        printf("Usage: whatsappServer clientName serverAddress serverPort\n");  //todo server shouldnt crash upon receiving illegal requests?
+        exit(1);
+    }
+
+    char * clientName = argv[1];
+    char * serverAddress = argv[2];
+    int portNumber = atoi(argv[3]);
+
+    //todo validate args!
+    if (portNumber < 0 || portNumber> 65535){
+        printf("Usage: whatsappServer clientName serverAddress serverPort\n");
+        exit(1);
+    }
+
     //// --- Setup  ---
 
     //// connect to specified port
